42db6d39623d9a1734d6d7f8a9e469957ced596a
alchu33 <alchu33@gmail.com>
2017-12-17 15:09:25 -0400

    removiendo error

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a550087ed7b37ead656cc21a46016328540ea8ed
 Route::get('SingUp','FormularioController@mostrarFormularioSingIn');
 Route::get('Login', 'FormularioController@mostrarFormularioLogin');
 
 //Es necesario colocar el post para manejar el boton submit
 Route::post('SingUp','FormularioController@manejarEventoCrearSesion');
 Route::post('Login', 'FormularioController@manejarEventoLogin');
-
