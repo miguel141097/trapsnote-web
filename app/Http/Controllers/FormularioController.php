280d306e14eb0548b772d8883ff3c456a3266b0f
Jorge Viloria <javiloria100@gmail.com>
2017-12-23 15:23:22 -0400

    solucion a conflictos

--- app/Http/Controllers/FormularioController.php
+++ app/Http/Controllers/FormularioController.php
@@ -35,41 +35,14 @@ class FormularioController extends Controller
 
     //La variable request contiene TODOS los valores ingresados por el usuario en el formulario
     public function manejarEventoCrearSesion(UserCreateRequest $request){
+     //Concatena la fecha
+     $fechaDeNacimiento = $request['year'] . '/' . $request['month'] . '/' . $request['day'];
+     $arregloConDatosDelUsuario = array( 'username'=>$request['username'],'name' => $request['name'], 'last_name' => $request['last_name'],'email' => $request['email'], 'password' => $request['password'],'password_repeat' => $request['password_repeat'], 'fechaDeNacimiento' => $fechaDeNacimiento);
+     //Esta clase maneja el envio de los datos por parte del usuario
+     $recurso = new \trapsnoteWeb\Libreria\RecursoHTTP();
+     $recurso->sendPostNuevoUsuario($arregloConDatosDelUsuario);
+ }
 
-    	$arregloConDatosDelUsuario = array( 'username'=>$request['username'],'name' => $request['name'], 'last_name' => $request['last_name'],'email' => $request['email'], 'password' => $request['password'],'password_repeat' => $request['password_repeat'] );
-
-    	//Convierte el arreglo con todos los datos en un JSON
-    	$JSON = json_encode($arregloConDatosDelUsuario);
-
-    	//URL de la base de datos en Heroku
-    	$url = 'https://dry-forest-40048.herokuapp.com/usuarios';
-
-    	//Crea un nuevo recurso cURL
-        $ch = curl_init($url);
-
-        //Si no hubo ningún error, se procede a enviar los datos al servidor
-        if($ch != false){
-
-        	//Configuración del recurso cURL
-	        curl_setopt($ch, CURLOPT_POST, 1);
-	        curl_setopt($ch, CURLOPT_POSTFIELDS, $JSON);
-	        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-
-	        // Captura la URL y envia a la base de datos
-	        curl_exec($ch);
-
-	        // Cierrar el recurso cURLy libera recursos del sistema
-			curl_close($ch);
-
-			echo "<br> Se ha registrado exitosamente ";
-
-		}
-		else{
-			echo "<br> Hubo problemas al enviar los datos al servidor";
-		}
-
-
-    }
 
     public function manejarEventoLogin(UserLoginRequest $request){
 
